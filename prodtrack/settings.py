cbb7c7f09f488905c3adfd75936e911f427490a4
Alexis Fuenmayor <afuenmayor1316@gmail.com>
2025-12-12 16:08:31 -0600

    Adding renderurl

--- prodtrack/settings.py
+++ prodtrack/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['productiontracker.dev', "www.productiontracker.dev",]
+ALLOWED_HOSTS = ['productiontracker.dev', "www.productiontracker.dev", 'https://warehouseproductiontracker.onrender.com']
 
 
 
